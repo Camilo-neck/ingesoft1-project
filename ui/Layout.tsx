bd6b6fb2a1c5a30d548f6f1664c3605f575f8f93
Camilo-neck <ccuello@unal.edu.co>
2022-11-04 12:04:22 -0500

    Design index Banner

--- ui/Layout.tsx
+++ ui/Layout.tsx
@@ -1,14 +1,12 @@
 import CNavBar from "./chazamNavBar"
 import Footer from "./Footer"
-import Image from "next/image"
-import styles from '@/styles/Home.module.css'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
 	return (
 		<div className="">
 			<CNavBar />
-			<main className={styles.main}>
+			<main className='min-h-screen pt-1 flex flex-1 flex-col'>
 				{children}
 			</main>
 			<Footer />
