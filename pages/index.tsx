c989a850d1e0d458dffe0216703a0553c5e63ac4
Camilo-neck <ccuello@unal.edu.co>
2022-11-04 10:06:58 -0500

    Add Banner tag to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import Layout from '@/ui/Layout'
+import Banner from '@/ui/Banner'
 
 const Home1: NextPage = () => {
 	return (
 		<Layout>
+			<Banner />
 		</Layout>
 	)
 }
