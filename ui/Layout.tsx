07b3f95e976dffbcb559ee1d84f0d7b3de7ed893
nquirogac <nquirogac@unal.edu.co>
2022-11-07 11:13:02 -0500

    Cards scroll

--- ui/Layout.tsx
+++ ui/Layout.tsx
@@ -13,8 +13,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 			<main className={styles.main}>
 				{children}
 				<SliderOfCards/>
-				
-    		
 			</main>
 			<Footer />
 		</div>
