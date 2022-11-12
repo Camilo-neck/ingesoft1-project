8fdbd46e2a16438449048c3b2ecddff8790ea135
Camilo <69314816+Camilo-neck@users.noreply.github.com>
2022-11-11 21:17:01 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
 import Layout from '@/ui/Layout'
 import Banner from '@/ui/Banner'
-import Register from './register'
-import Login from './login'
+
 const Home1: NextPage = () => {
 	return (
 		<Layout>
-			<Login />
+			<Banner />
 		</Layout>
 	)
 }
